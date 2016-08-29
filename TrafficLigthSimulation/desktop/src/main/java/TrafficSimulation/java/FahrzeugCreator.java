5873a5d61bc359e0dec5d7edbc4487c8cd6729a0
TranKhacDat <khac.dat.tran@tu-clausthal.de>
2016-08-30 04:48:11 +0700

    Update FahrzeugCreator.java

--- TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/FahrzeugCreator.java
+++ TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/FahrzeugCreator.java
@@ -1,13 +1,40 @@
+package TrafficSimulation.java;
+
+/**
+ * 
+ * 
+ * @author Khac Dat Tran
+ * 
+ */
+
 public class FahrzeugCreator {
 
- public Fahrzeug createFahrzeug () {
- 
-    if (model.getViewEastIn()[0]=='o') return new QueueEast ();
- 
-     else if (model.getViewWestIn()[0]=='o') return new QueueWest ();
+	private CentralModel model;
 
-     else if (model.getViewNorthIn()[0]=='o') return new QueueNorth ();
- 
-     else (model.getViewSouthIn()[0]=='o') return new QueueSouth ();
-    }
- }
+	public FahrzeugCreator() {
+
+	}
+
+	public Fahrzeug createFahrzeug() {
+
+		if (model.getViewEastIn()[0] == 'o') {
+			return new QueueEast();
+		}
+
+		else if (model.getViewWestIn()[0] == 'o') {
+			return new QueueWest();
+		}
+
+		else if (model.getViewNorthIn()[0] == 'o') {
+			return new QueueNorth();
+		}
+
+		else if (model.getViewSouthIn()[0] == 'o') {
+			return new QueueSouth();
+		}
+
+		return null;
+
+	}
+
+}
