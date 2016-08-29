aa5077fa96f8e1cf0f903e2b22b79c9f97efd5aa
TranKhacDat <khac.dat.tran@tu-clausthal.de>
2016-08-30 04:37:01 +0700

    Update QueueSouth.java

--- TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/QueueSouth.java
+++ TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/QueueSouth.java
@@ -1,3 +1,14 @@
-public class QueueSouth{
-	
+package TrafficSimulation.java;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * 
+ * 
+ * @author Khac Dat Tran
+ * 
+ */
+
+public class QueueSouth implements Fahrzeug{
+	private ConcurrentLinkedQueue<CarController> carQueueSouth;
 }
