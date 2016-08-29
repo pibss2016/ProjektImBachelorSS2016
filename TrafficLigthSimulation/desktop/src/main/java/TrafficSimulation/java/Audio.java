7d0269a8fe1a89f9c53f1261219ace672dad5344
TranKhacDat <khac.dat.tran@tu-clausthal.de>
2016-08-30 04:41:50 +0700

    Update Audio.java

--- TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/Audio.java
+++ TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/Audio.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.files.FileHandle;
 
+/**
+ * 
+ * 
+ * @author Khac Dat Tran
+ * 
+ */
+
 public interface Audio {
 	
 	public AudioDevice newAudioDevice (int samplingRate, boolean isMono);
