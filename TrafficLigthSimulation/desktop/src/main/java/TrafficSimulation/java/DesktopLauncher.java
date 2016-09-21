ee15a2b3545c84b800ec830a3115d38da954e5bf
TranKhacDat <khac.dat.tran@tu-clausthal.de>
2016-09-21 13:49:47 +0700

    Update DesktopLauncher.java

--- TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/DesktopLauncher.java
+++ TrafficLigthSimulation/desktop/src/main/java/TrafficSimulation/java/DesktopLauncher.java
@@ -13,10 +13,23 @@ import TrafficSimulation.java.MainController;
  */
 
 public class DesktopLauncher {
+	
 	public static void main (String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		
+	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Trafficlight Simulation";
         
+        int laneLength = 10;//Laenge der Lanes, muss >= 5 sein
+        int waitingTime = 500;//Verzoegerung des Animationsablauf in Millisekunden; je kleiner waitingTime, desto höhere Geschwindigkeit von Autos
+        int carSpawnRate = 3;// Anzahl der Iterationen der Simulation die zwischen dem Generieren von 2 Autos durchgefuert werden; je kleiner carSpawnRate, desto mehr Autos erscheinen, aber es darf nicht kleiner als 1 sein
+        
+        cfg.width = ((laneLength*2)+2)*32;
+        cfg.height = ((laneLength*2)+2)*32;
+
+        new LwjglApplication(new MainController(laneLength ,waitingTime,carSpawnRate), cfg);
+        new Thread(new MainController(laneLength,waitingTime,carSpawnRate)).start();
+        
+        /*
         //Yaml-Datei wird eingelesen
         //rushHour.yml = viele Autos, hohe Geschwindigkeit
         //normalTraffic.yml = weniger Autos, geringe Geschwindigkeit
@@ -32,17 +45,7 @@ public class DesktopLauncher {
 	int laneLength = (int)map.get("laneLength"); //Laenge der Lanes, muss >= 5 sein
 	int waitingTime = (int)map.get("waitingTime");//Verzoegerung des Animationsablauf in Millisekunden; je kleiner waitingTime, desto höhere Geschwindigkeit von Autos
 	int carSpawnRate = (int)map.get("carSpawnRate");// Anzahl der Iterationen der Simulation die zwischen dem Genrieren von 2 Autos durchgefuert werden; je kleiner carSpawnRate, desto mehr Autos erscheinen, aber es darf nicht kleiner als 1 sein
-        
-        /**
-        int laneLength = 10;//Laenge der Lanes, muss >= 5 sein
-        int waitingTime = 500;//Verzoegerung des Animationsablauf in Millisekunden; je kleiner waitingTime, desto höhere Geschwindigkeit von Autos
-        int carSpawnRate = 3;// Anzahl der Iterationen der Simulation die zwischen dem Genrieren von 2 Autos durchgefuert werden; je kleiner carSpawnRate, desto mehr Autos erscheinen, aber es darf nicht kleiner als 1 sein
         */
-        cfg.width = ((laneLength*2)+2)*32;
-        cfg.height = ((laneLength*2)+2)*32;
-
-        new LwjglApplication(new MainController(laneLength ,waitingTime,carSpawnRate), cfg);
-        new Thread(new MainController(laneLength,waitingTime,carSpawnRate)).start();
         
 	}
 }
